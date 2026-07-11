beaa13df44af3e95162c9fd49051622ea0282e58
Yefersn15 <yefersonandres225@gmail.com>
2026-07-11 00:17:04 -0500

    pequeño cambio

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from './App.jsx';
