baa85689ec7173f290f62e046424dca7e0efd4f8
Yefersn15 <yefersonandres225@gmail.com>
2026-07-11 00:49:10 -0500

    intentento2

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,8 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
